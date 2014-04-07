ec3321f79a921e7c165ba9aab00a7d1eb4923d38
altazure <aatu.liimatta@gmail.com>
2014-04-07 20:58:04 +0300

    AssemblyInfo: Updated

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QRer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ClipQR")]
+[assembly: AssemblyDescription("Clipboard QR generator")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("QRer")]
+[assembly: AssemblyProduct("ClipQR")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
